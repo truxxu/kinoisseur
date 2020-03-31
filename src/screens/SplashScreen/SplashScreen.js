7376e9631f171e32d894f48b3fcf260bc753133c
Daniel Trujillo <ganimedes@firemail.cc>
2020-03-30 22:15:43 -0500

    refactored for logic/view separation

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useRef }  from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-  Image,
   Animated
 } from 'react-native';
-import logo from '../../assets/clapperboard.png'
+import SplashView from './SplashView'
 
 const SplashScreen = ({ navigation }) => {
 
@@ -29,54 +24,9 @@ const SplashScreen = ({ navigation }) => {
     }).start();
   };
 
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Animated.View
-        style={[
-          styles.fadingView,
-          {
-            opacity: fadeAnim
-          }
-        ]}
-      >
-        <Image
-          style={styles.logo}
-          source={logo}
-        />
-
-        <Text style={styles.title}>
-          Kinoisseur
-        </Text>
-      </Animated.View>
-    </SafeAreaView>
+    <SplashView animation={fadeAnim} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#08d9d6'
-  },
-
-  fadingView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    fontSize: 55,
-    color: '#ff2e63',
-  },
-
-  logo: {
-    width: 100,
-    height: 100,
-  },
-});
 
 export default SplashScreen;
---
+++ src/screens/SplashScreen/SplashView.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Animated
+} from 'react-native';
+import logo from '../../assets/clapperboard.png'
+
+const SplashView = (props) => {
+
+  const { animation } = props;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fadingView,
+          {
+            opacity: animation
+          }
+        ]}
+      >
+        <Image
+          style={styles.logo}
+          source={logo}
+        />
+
+        <Text style={styles.title}>
+          Kinoisseur
+        </Text>
+      </Animated.View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#08d9d6'
+  },
+
+  fadingView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    fontSize: 55,
+    color: '#ff2e63',
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default SplashView;
