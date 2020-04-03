d92d502ee0e552bb9dff0ec5b091163147f03b7a
Daniel Trujillo <ganimedes@firemail.cc>
2020-04-03 00:33:23 -0500

    adds dummy card to genre list

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,16 +1,64 @@
 import React from 'react';
-import {View, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 
 import Navbar from '../../common/Navbar';
 import GenreCard from '../../common/GenreCard';
 
+const WIDTH = Dimensions.get('window').width;
+
 const HomeScreen = ({navigation}) => {
+  const _data = [
+    {
+      title: 'Action',
+      url: 'https://i.picsum.photos/id/440/300/200.jpg',
+    },
+    {
+      title: 'Romance',
+      url: 'https://i.picsum.photos/id/440/300/200.jpg',
+    },
+    {
+      title: 'Comedy',
+      url: 'https://i.picsum.photos/id/440/300/200.jpg',
+    },
+    {
+      title: 'Sci-Fi',
+      url: 'https://i.picsum.photos/id/440/300/200.jpg',
+    },
+    {
+      title: 'Drama',
+      url: 'https://i.picsum.photos/id/440/300/200.jpg',
+    },
+  ];
+  const data = _data || [];
+
+  const generateCards = () => {
+    const cards = data.map((item, idx) => {
+      return (
+        <GenreCard key={idx} navigation={navigation} _title={item.title} />
+      );
+    });
+
+    if (data.length % 2 === 1) {
+      return cards.concat(<View key={data.length} style={styles.dummycard} />);
+    } else {
+      return cards;
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Navbar navigation={navigation} _title="Kinoisseur" />
       <View style={styles.content}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <GenreCard navigation={navigation} _title="Action" />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollview}>
+          {generateCards()}
         </ScrollView>
       </View>
     </SafeAreaView>
@@ -26,6 +74,16 @@ const styles = StyleSheet.create({
   content: {
     flex: 8,
     padding: 5,
+  },
+  scrollview: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  dummycard: {
+    width: WIDTH * 0.4,
+    height: 75,
+    margin: 7.5,
   },
 });
 
